307624f73b4ac5561abb30c6b41bf97ad14089d8
HenarDoel <henardoel@gmail.com>
2020-08-26 13:52:07 +0200

    in labelme_to_dataset created a loop to iterate over all the objects in the image and stracted them

--- labelme_to_dataset.py
+++ labelme_to_dataset.py
@@ -15,16 +15,21 @@ with open('images/Deadpool 006-007.json') as f:
 img_bytes = base64.b64decode(data['imageData']) # saco de la string los bytes en base 64
 image = Image.open(io.BytesIO(img_bytes)) # los convierto a imagen
 image_array=np.array(image)
-detections=data['shapes']
-
-# sacamos las coordenadas de la primera bounding box
-# remember: array de numpy es (fila, columna), pero las cooridnadas de la bounding box
-# vienen con formato de imagen (columna, fila)
-ymin=round(detections[0]["points"][0][1])
-ymax=round(detections[0]["points"][1][1])
-xmin=round(detections[0]["points"][0][0])
-xmax=round(detections[0]["points"][1][0])
+detections_coords=data['shapes']
+# creo una lista para almacenar solo las detecciones
+detections = []
+# iteramos sobre todas las deteciones que tenga la imagen
+for dc in detections_coords:
+    # sacamos las coordenadas de la primera bounding box
+    # remember: array de numpy es (fila, columna), pero las cooridnadas de la bounding box
+    # vienen con formato de imagen (columna, fila)
+    ymin = round(dc["points"][0][1])
+    ymax = round(dc["points"][1][1])
+    xmin = round(dc["points"][0][0])
+    xmax = round(dc["points"][1][0])
+    # sacamos la deteccion actual y la añadimos a la lista de detecciones
+    detections.append(image_array[ymin:ymax, xmin:xmax])
 
 # mostramos la imagen recortada, solo con la bounding box
 # (es un numpy array, por eso usamos io2.show)
-io2.imshow(image_array[ymin:ymax,xmin:xmax])
+#io2.imshow(image_array[ymin:ymax,xmin:xmax])
